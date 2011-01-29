1ec2900a7ccc8048b845ad1f62b0b71580901379
Ivan Krechetov <ivan.krechetov@gmail.com>
2011-01-29 23:50:10 +0100

    Removed the evaluations

--- recursion-and-iteration.scm
+++ recursion-and-iteration.scm
@@ -5,12 +5,3 @@
             (f-recursive (- n 1))
             (* 2 (f-recursive (- n 2)))
             (* 3 (f-recursive (- n 3))))))
-
-(f-recursive 1)
-(f-recursive 2)
-(f-recursive 3)
-(f-recursive 4)
-(f-recursive 5)
-(f-recursive 10)
-(f-recursive 20)
-(f-recursive 30)
