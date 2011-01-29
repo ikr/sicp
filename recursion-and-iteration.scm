7f3847cd1e5719f1638d3731c6184e9ce432ed35
Ivan Krechetov <ivan.krechetov@gmail.com>
2011-01-29 23:41:18 +0100

    f-recursive corrected

--- recursion-and-iteration.scm
+++ recursion-and-iteration.scm
@@ -1,17 +1,16 @@
 (define (f-recursive n)
-	(if (< n 3)
-		(+
-			(f-recursive (- n 1))
-			(* 2 (f-recursive (- n 2)))
-			(* 3 (f-recursive (- n 3)))))
+    (if (< n 3)
+        n
+        (+
+            (f-recursive (- n 1))
+            (* 2 (f-recursive (- n 2)))
+            (* 3 (f-recursive (- n 3))))))
 
 (f-recursive 1)
 (f-recursive 2)
 (f-recursive 3)
 (f-recursive 4)
 (f-recursive 5)
-(f-recursive 6)
-(f-recursive 7)
-(f-recursive 8)
-(f-recursive 9)
 (f-recursive 10)
+(f-recursive 20)
+(f-recursive 30)
