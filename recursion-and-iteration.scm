00c45d527257c3f3bb5c93fc1c5a695683214046
Ivan Krechetov <ivan.krechetov@gmail.com>
2011-01-30 09:59:12 +0100

    Implemented f-iterative

--- recursion-and-iteration.scm
+++ recursion-and-iteration.scm
@@ -5,3 +5,26 @@
             (f-recursive (- n 1))
             (* 2 (f-recursive (- n 2)))
             (* 3 (f-recursive (- n 3))))))
+
+(define (f-0 f-1 f-2 f-3)
+    (+
+        f-1
+        (* 2 f-2)
+        (* 3 f-3)))
+
+(define (f-n n f-1 f-2 f-3)
+    (if (< n 3)
+        n
+        (f-0 f-1 f-2 f-3)))
+
+(define (f n-final n-current f-1 f-2 f-3)
+    (if (= n-current n-final)
+        (f-n n-current f-1 f-2 f-3)
+        (f
+            n-final
+            (+ n-current 1)
+            (f-n n-current f-1 f-2 f-3)
+            f-1
+            f-2)))
+
+(define (f-iterative n) (f n 0 0 0 0))
