e4021f9df89f804f8d570a2103106e888e91b31f
Ivan Krechetov <ivan.krechetov@gmail.com>
2013-01-02 19:05:17 +0100

    Solution for 1.19

--- excercises/chapter_1/1.19.scm
+++ excercises/chapter_1/1.19.scm
@@ -1,7 +1,7 @@
 ; Q: There is a clever algorithm for computing the Fibonacci numbers in a logarithmic number of
 ; steps. Recall the transformation of the state variables a and b in the fib-iter process
-; ofSection 1.2.2: a ← a + b and b ← a. Call this transformation T, and observe that applying T over
-; and over again n times, starting with 1 and 0, produces the pair Fib(n + 1) and Fib(n).
+; of Section 1.2.2: a ← a + b and b ← a. Call this transformation T, and observe that applying T
+; over and over again n times, starting with 1 and 0, produces the pair Fib(n + 1) and Fib(n).
 
 ; In other words, the Fibonacci numbers are produced by applying T^n, the n-th power of the
 ; transformation T, starting with the pair (1, 0). Now consider T to be the special case of p = 0
@@ -14,6 +14,28 @@
 ; squaring, as in the fast-expt procedure. Put this all together to complete the following
 ; procedure, which runs in a logarithmic number of steps:
 
+; (define (fib-iter a b p q count)
+;     (cond
+;         ((= count 0) b)
+;         ((even? count)
+;             (fib-iter
+;                 a
+;                 b
+;                 〈??〉    ; compute p′
+;                 〈??〉    ; compute q′
+;                 (/ count 2)))
+;         (else
+;             (fib-iter
+;                 (+ (* b q) (* a q) (* a p))
+;                 (+ (* b p) (* a q))
+;                 p
+;                 q
+;                 (- count 1)))))
+
+; A: See 1.19.odf (LibreOffice Math file) for Tp′q′ derivation. Now, using that result:
+
+(define (square x) (* x x))
+
 (define (fib-iter a b p q count)
     (cond
         ((= count 0) b)
@@ -21,8 +43,8 @@
             (fib-iter
                 a
                 b
-                0   ; compute p′
-                0   ; compute q′
+                (+ (square p) (square q))   ; compute p′
+                (+ (* 2 p q) (square q))    ; compute q′
                 (/ count 2)))
         (else
             (fib-iter
@@ -41,3 +63,8 @@
 (fib 4)
 (fib 5)
 (fib 6)
+(fib 7)
+(fib 8)
+(fib 9)
+(fib 10)
+(fib 40)
