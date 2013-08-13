e9f9db2cb05e67f694aac02a68de243934689259
Ivan Krechetov <ivan.krechetov@gmail.com>
2013-08-13 20:41:55 +0200

    Finalize 1.20

--- excercises/chapter_1/1.20.scm
+++ excercises/chapter_1/1.20.scm
@@ -59,3 +59,22 @@
       (remainder
         (remainder 40 (remainder 206 40))
         (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))))
+
+(remainder (remainder 206 40) (remainder 40 (remainder 206 40))) ; 4 calls
+
+; In total, 18 "remainder" calls are made during the normal-order evaluation.
+
+
+; 2. Applicative-order evaluation
+
+(gcd 206 40)
+
+(gcd 40 (remainder 206 40))
+
+(gcd 6 (remainder 40 6))
+
+(gcd 4 (remainder 6 4))
+
+(gcd 2 (remainder 4 2))
+
+; In total, 4 "remainder" calls are made during the applicative-order evaluation.
