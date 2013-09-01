a77ecf7a85e4bd331c76bd633bc24f4f23ef79bd
Ivan Krechetov <ivan.krechetov@gmail.com>
2013-09-01 19:45:29 +0200

    The new measurements

--- exercises/chapter_1/1.23.scm
+++ exercises/chapter_1/1.23.scm
@@ -9,3 +9,50 @@
 ; number of test steps, you should expect it to run about twice as fast. Is this expectation
 ; confirmed? If not, what is the observed ratio of the speeds of the two algorithms, and how do you
 ; explain the fact that it is different from 2?
+
+(define (prime? n) (= n (smallest-divisor n)))
+
+(define (smallest-divisor n) (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond
+    ((> (square test-divisor) n) n)
+    ((divides? test-divisor n) test-divisor)
+    (else (find-divisor n (next test-divisor)))))
+
+(define (divides? a b) (= (remainder b a) 0))
+
+(define (next a) (if (= a 2) 3 (+ a 2)))
+
+;---------------------------------------------------------------------------------------------------
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+    (report-prime (- (runtime) start-time))))
+
+(define (report-prime elapsed-time)
+  (display " ")
+  (display elapsed-time))
+
+(timed-prime-test 100000000003)
+(timed-prime-test 100000000019)
+(timed-prime-test 100000000057) ; was: ~0.6 now: ~0.36
+
+(timed-prime-test 1000000000039)
+(timed-prime-test 1000000000061)
+(timed-prime-test 1000000000063) ; was: ~1.8 now: ~1.15
+
+(timed-prime-test 10000000000037)
+(timed-prime-test 10000000000051)
+(timed-prime-test 10000000000099) ; was: ~5.7 now: ~3.57
+
+(timed-prime-test 100000000000031)
+(timed-prime-test 100000000000067)
+(timed-prime-test 100000000000097) ; was: ~18.1 now: ~11.35
+
+; A:
