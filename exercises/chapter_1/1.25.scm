1412f5c5863e26cd14c41bc6f7253e134c30db9b
Ivan Krechetov <ivan.krechetov@gmail.com>
2014-03-23 18:19:02 +0100

    The answer statement for 1.25

--- exercises/chapter_1/1.25.scm
+++ exercises/chapter_1/1.25.scm
@@ -29,3 +29,10 @@
 (with-timings
     (lambda () (expmod 123071 250993 250993))
     report)
+
+;; A: No, she's not. As the example above demonstrates, the proposed "expmod" is way slower than the
+;; original expmod-0 (5.977 seconds vs. under 0.001 seconds). Without the "remainder" on each
+;; recursion step, keeping the numbers within the modulo-m field, all the arithmetic grows very fast
+;; out of the processor word boundaries into the BigNum domain, and all the operations become
+;; "non-elementary". Which means that the time required to subtract, multiply and divide those big
+;; numbers grows with the numbers themselves.
