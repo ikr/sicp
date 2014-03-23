cf87518fe525d46f659d0614f95e009cc1817e5d
Ivan Krechetov <ivan.krechetov@gmail.com>
2014-03-23 18:29:56 +0100

    Formatting

--- exercises/chapter_1/1.24.scm
+++ exercises/chapter_1/1.24.scm
@@ -1,8 +1,8 @@
-; Q: Modify the timed-prime-test procedure of exercise 1.22 to use fast-prime? (the Fermat method),
-; and test each of the 12 primes you found in that exercise. Since the Fermat test has Θ(log n)
-; growth, how would you expect the time to test primes near 1,000,000 to compare with the time
-; needed to test primes near 1000? Do your data bear this out? Can you explain any discrepancy you
-; find?
+;; Q: Modify the timed-prime-test procedure of exercise 1.22 to use fast-prime? (the Fermat method),
+;; and test each of the 12 primes you found in that exercise. Since the Fermat test has Θ(log n)
+;; growth, how would you expect the time to test primes near 1,000,000 to compare with the time
+;; needed to test primes near 1000? Do your data bear this out? Can you explain any discrepancy you
+;; find?
 
 (define (expmod base exp m)
   (cond
@@ -20,7 +20,7 @@
     ((fermat-test n) (fast-prime? n (- times 1)))
     (else false)))
 
-;---------------------------------------------------------------------------------------------------
+;;--------------------------------------------------------------------------------------------------
 
 (define (measure n)
   (write n)
